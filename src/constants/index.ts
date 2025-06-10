d42d43d68fa64849ab86baa910d0096832f02500
Mohammed Ziyan <mhdziyannn@gmail.com>
2025-06-10 23:58:28 +0530

    Delete src directory

--- src/constants/index.ts
+++
@@ -1,3 +0,0 @@
-export const LEAD_STATUSES = ['new', 'contacted', 'qualified', 'lost'] as const;
-export const USER_ROLES = ['admin', 'sales'] as const;
-export const LEAD_SOURCES = ['referral', 'ads', 'website', 'other'] as const;
